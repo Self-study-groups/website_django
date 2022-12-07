4db9ebddc01a37c3bc36185428de7b85f723a27f
UsernameAnda <piccoaldi@gmail.com>
2022-12-08 01:13:15 +0700

    first

---
+++ config/settings/development.py
@@ -0,0 +1,18 @@
+from base import *
+
+DEBUG = True
+
+# Database Config
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Override Installer Apps
+
+INSTALLED_APPS +=[
+
+]
